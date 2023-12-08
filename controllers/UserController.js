11f8d2537d69b1fa2e09323fcccfb67fb9cc9dec
Rafael Rodrigues <rafaelkunzler@gmail.com>
2023-12-08 09:23:23 -0300

    get user by id

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -124,9 +124,34 @@ const update = async(req, res) => {
 
 }
 
+//get user by id
+const getUserById = async(req, res) => {
+    const {id} = req.params
+
+    try {
+        const user = await User.findById(id).select("-password")
+
+        //check if user exists
+        if(!user) {
+            res.status(404).json({errors: ["Usuário não encontrado"]})
+            return
+        }
+
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(404).json({errors: ["Usuário não encontrado"]})
+        return
+    }
+
+    
+
+    
+}
+
 module.exports = {
     register,
     login,
     getCurrentUser,
     update,
+    getUserById
 }
